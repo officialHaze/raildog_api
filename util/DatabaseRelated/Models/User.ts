b649ec659f5746d33bd1c038a243e3716ea4b84d
Assassin <moinak.dey8@gmail.com>
2023-12-19 00:51:08 +0530

    User and admin model

---
+++ util/DatabaseRelated/Models/User.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose"
+
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    phone: {
+      type: Number,
+      required: true,
+      unique: true
+    },
+    is_verified: {
+      type: Boolean,
+      required: true
+    },
+    // Role can be of 3 types only - Individual, Developer, Business
+    role: {
+      type: String,
+      required: true
+    },
+    business_name: {
+      type: String,
+    },
+    verification_code: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true
+  }
+)
+
+const User = mongoose.model("User", UserSchema)
+
+export default User
+
