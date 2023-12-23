9ac792fef92e070ac26736f056628971fd30f012
Assassin <moinak.dey8@gmail.com>
2023-12-24 00:13:10 +0530

    Send appropriate response for user registration

--- util/Controllers/RouteController.ts
+++ util/Controllers/RouteController.ts
@@ -9,38 +9,38 @@ export default class RouteController {
     try {
       const userData: UserData = req.body;
       console.log("User data for registration: ", userData);
-      
+
       // Check if user already exists
-      const user = await User.findOne({email: userData.email});
-      if(user) {
-        res.status(400).json({Error: "User is already registered!"});
+      const user = await User.findOne({ email: userData.email });
+      if (user) {
+        res.status(400).json({ Error: "User is already registered!" });
         return;
       }
 
       // Validate the user data
       const isValidEmail = Validator.validateEmail(userData.email);
       console.log("Email is valid? ", isValidEmail);
-      
+
       const isValidPhone = Validator.validatePhone(userData.phone);
       console.log("Is valid phone number? ", isValidPhone);
 
-      if(!isValidEmail) {
-        res.status(400).json({Error: "Invalid email!"});
+      if (!isValidEmail) {
+        res.status(400).json({ Error: "Invalid email!" });
         return;
       }
 
-      if(!isValidPhone) {
-        res.status(400).json({Error: "Invalid phone number!"});
+      if (!isValidPhone) {
+        res.status(400).json({ Error: "Invalid phone number!" });
         return;
       }
 
-      if(!userData.role) {
-        res.status(400).json({Error: "No role provided!"});
+      if (!userData.role) {
+        res.status(400).json({ Error: "No role provided!" });
         return;
       }
 
-      if(userData.username.length > 20) {
-        res.status(400).json({Error: "Username maximum length exceeded!"});
+      if (userData.username.length > 20) {
+        res.status(400).json({ Error: "Username maximum length exceeded!" });
         return;
       }
 
@@ -48,11 +48,10 @@ export default class RouteController {
       const db = new DB();
       await db.createUser(userData);
 
-      res.status(200).json({message: "User created!"});
-    }
-    catch(err) {
-      console.error(err)
-      res.status(500).json({Error: "server error!"});
+      res.status(200).json({ message: "User created!" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ Error: "server error!" });
     }
   }
 }
