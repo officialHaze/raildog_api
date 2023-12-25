812bd518c57eb0d05ddaff08ae3e28a4b4c40b05
Assassin <moinak.dey8@gmail.com>
2023-12-25 16:19:39 +0530

    Account verification

--- server.ts
+++ server.ts
@@ -35,6 +35,7 @@ class RailDog {
     this.app.post("/get_trains", this.findTrains);
     this.app.post("/get_captcha_image", this.getCaptchaImage);
     this.app.post("/register", RouteController.userRegistration);
+    this.app.get("/activate/:activationToken", RouteController.activateAccount);
 
     // TEST ROUTES
     this.app.get("/test-mail", RouteController.testMail);
